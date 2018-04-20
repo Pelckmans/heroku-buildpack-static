ebd8568950af34893346c1e894f104b959b0121b
Tom Van Haver <tom.vanhaver@euri.com>
2018-04-20 15:26:34 +0200

    add header value interpolation

--- scripts/config/lib/ngx_mruby/headers.rb
+++ scripts/config/lib/ngx_mruby/headers.rb
@@ -17,7 +17,7 @@ if config["headers"]
         print 'in headers.rb => ENV =>', ENV["SOME_VAR"]
         print 'in headers.rb => interpolate result =>', NginxConfigUtil.interpolate(value.to_s, ENV)
 
-        req.headers_out[key] = NginxConfigUtil.interpolate(value, ENV).to_s
+        req.headers_out[key] = NginxConfigUtil.interpolate(value.to_s, ENV)
       end
       break
     end
